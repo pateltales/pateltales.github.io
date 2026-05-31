c804ed8649deb895d2f4e89dc5dba7b6e217de02
pateltales <pateltalesconnect@gmail.com>
2026-05-30 19:36:28 -0700

    SMA RUn May 30

--- galleries-data.js
+++ galleries-data.js
@@ -18,6 +18,7 @@
 const S3_BASE = 'https://pateltales-photography.s3.us-east-2.amazonaws.com';
 
 const GALLERIES = [
+    { id: 'sma-run-05302026', title: 'SMA Run May 30th 2026', cover: '20260530-DSC09306.jpg', category: 'general' },
   { id: 'sma-2026', title: 'Seattle Marathon Ambassadors', cover: '20260522-DSC08782.jpg', category: 'general' },
   { id: 'portraits/gaga', title: 'Yulha', cover: '20260315-DSC05629.jpg', category: 'portrait' },
   { id: 'portraits/yasab-feser', title: 'Yasab', cover: '20260315-DSC05854.jpg', category: 'portrait' },
